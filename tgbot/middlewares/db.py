af6b0b4a50af180ff5eb7e0a711a2cfc6e03afa6
latand <latand666@gmail.com>
2021-10-03 09:37:51 +0300

    fix returning user

--- tgbot/middlewares/db.py
+++ tgbot/middlewares/db.py
@@ -12,12 +12,12 @@ class DbMiddleware(LifetimeControllerMiddleware):
         telegram_user: types.User = obj.from_user
         user = await User.get_user(session_maker=session_maker, telegram_id=telegram_user.id)
         if not user:
-            await User.add_user(session_maker,
-                                telegram_user.id, first_name=telegram_user.first_name,
-                                last_name=telegram_user.last_name,
-                                username=telegram_user.username,
-                                lang_code=telegram_user.language_code,
-                                role='user'
-                                )
+            user = await User.add_user(session_maker,
+                                       telegram_user.id, first_name=telegram_user.first_name,
+                                       last_name=telegram_user.last_name,
+                                       username=telegram_user.username,
+                                       lang_code=telegram_user.language_code,
+                                       role='user'
+                                       )
 
         data['user'] = user
